c548408011d2963638e0238af300783bbf80ed52
ProfessorPitch <kingpitch@gmail.com>
2018-07-31 14:28:12 -0400

    New Files and Updates

--- vagrant/catalog/finalProject.py
+++ vagrant/catalog/finalProject.py
@@ -1,46 +1,37 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import SingletonThreadPool
 from database_setup import Base, Restaurant, MenuItem
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db', poolclass=SingletonThreadPool)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-
-# Fake Restaurants
-# restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-# restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-# Fake Menu Items
-# items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-# item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-# items = []
-
-
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
-    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    restaurant_id = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(
         restaurant_id=restaurant_id).all()
+    session.close()
     return jsonify(MenuItems=[i.serialize for i in items])
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/JSON')
 def menuItemJSON(restaurant_id, menu_id):
     Menu_Item = session.query(MenuItem).filter_by(id=menu_id).one()
+    session.close()
     return jsonify(Menu_Item=Menu_Item.serialize)
 
 
 @app.route('/restaurant/JSON')
 def restaurantsJSON():
     restaurants = session.query(Restaurant).all()
+    session.close()
     return jsonify(restaurants=[r.serialize for r in restaurants])
 
 
@@ -50,6 +41,7 @@ def restaurantsJSON():
 def showRestaurants():
     restaurants = session.query(Restaurant).all()
     # return "This page will show all my restaurants"
+    session.close()
     return render_template('restaurants.html', restaurants=restaurants)
 
 
@@ -60,8 +52,10 @@ def newRestaurant():
         newRestaurant = Restaurant(name=request.form['name'])
         session.add(newRestaurant)
         session.commit()
+        session.close()
         return redirect(url_for('showRestaurants'))
     else:
+        session.close()
         return render_template('newRestaurant.html')
     # return "This page will be for making a new restaurant"
 
@@ -77,9 +71,10 @@ def editRestaurant(restaurant_id):
             editedRestaurant.name = request.form['name']
             return redirect(url_for('showRestaurants'))
     else:
+        session.close()
         return render_template(
             'editRestaurant.html', restaurant=editedRestaurant)
-
+    
     # return 'This page will be for editing restaurant %s' % restaurant_id
 
 # Delete a restaurant
@@ -92,9 +87,11 @@ def deleteRestaurant(restaurant_id):
     if request.method == 'POST':
         session.delete(restaurantToDelete)
         session.commit()
+        session.close()
         return redirect(
             url_for('showRestaurants', restaurant_id=restaurant_id))
     else:
+        session.close()
         return render_template(
             'deleteRestaurant.html', restaurant=restaurantToDelete)
     # return 'This page will be for deleting restaurant %s' % restaurant_id
@@ -107,6 +104,7 @@ def showMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(
         restaurant_id=restaurant_id).all()
+    session.close()
     return render_template('menu.html', items=items, restaurant=restaurant)
     # return 'This page is the menu for restaurant %s' % restaurant_id
 
@@ -125,8 +123,8 @@ def newMenuItem(restaurant_id):
         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
-
-    return render_template('newMenuItem.html', restaurant=restaurant)
+    session.close()
+    return render_template('newMenuItem.html', restaurant_id=restaurant_id)
     # return 'This page is for making a new menu item for restaurant %s'
     # %restaurant_id
 
@@ -148,9 +146,10 @@ def editMenuItem(restaurant_id, menu_id):
             editedItem.course = request.form['course']
         session.add(editedItem)
         session.commit()
+        session.close()
         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
     else:
-
+        session.close()
         return render_template(
             'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
@@ -166,9 +165,11 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
+        session.close()
         return redirect(url_for('showMenu', restaurant_id=restaurant_id))
     else:
-        return render_template('deleteMenuItem.html', item=itemToDelete)
+        session.close()
+        return render_template('deleteMenuItem.html',restaurant_id=restaurant_id, menu_id=menu_id, item=itemToDelete)
     # return "This page is for deleting menu item %s" % menu_id
 
 
